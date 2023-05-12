df4c8eceff046f1b3e389f3160b64f42925d7b61
Sursulet <57486825+Sursulet@users.noreply.github.com>
2023-05-12 14:43:11 +0200

    Create DummyMeetingApiService.java

---
+++ app/src/main/java/com/seurs/mareu/service/DummyMeetingApiService.java
@@ -0,0 +1,15 @@
+package com.seurs.mareu.service;
+
+import com.seurs.mareu.model.Meeting;
+
+import java.util.List;
+
+public class DummyMeetingApiService implements MeetingApiService {
+
+    private List<Meeting> mMeetings = DummyMeetingGenerator.generateMeetings();
+
+    @Override
+    public List<Meeting> getMeetings() {
+        return mMeetings;
+    }
+}
