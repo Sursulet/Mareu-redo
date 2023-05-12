99bfa4ce48d9ea55e5124d1b6b6ba84d78a17beb
Sursulet <57486825+Sursulet@users.noreply.github.com>
2023-05-12 14:07:20 +0200

    Update Meeting.java

--- app/src/main/java/com/seurs/mareu/model/Meeting.java
+++ app/src/main/java/com/seurs/mareu/model/Meeting.java
@@ -7,4 +7,45 @@ public class Meeting {
     String place;
     String topic;
     List<String> participants;
+
+    public Meeting(String hour, String place, String topic, List<String> participants) {
+        this.hour = hour;
+        this.place = place;
+        this.topic = topic;
+        this.participants = participants;
+    }
+
+    // Getter
+    public String getHour() {
+        return hour;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public List<String> getParticipants() {
+        return participants;
+    }
+
+    // Setter
+    public void setHour(String hour) {
+        this.hour = hour;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public void setParticipants(List<String> participants) {
+        this.participants = participants;
+    }
 }
