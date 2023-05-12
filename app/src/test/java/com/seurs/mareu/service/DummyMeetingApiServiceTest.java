2c4a3e54643d4a8822c8d040b4478b4cdf3f9b3c
Sursulet <57486825+Sursulet@users.noreply.github.com>
2023-05-12 15:00:30 +0200

    Create DummyMeetingApiServiceTest.java

---
+++ app/src/test/java/com/seurs/mareu/service/DummyMeetingApiServiceTest.java
@@ -0,0 +1,15 @@
+package com.seurs.mareu.service;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DummyMeetingApiServiceTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void getMeetings() {
+    }
+}
