71482bd8be1160386ecab427a3835156d84a6fed
Sursulet <57486825+Sursulet@users.noreply.github.com>
2023-05-15 17:27:28 +0200

    Update MeetingApiService.java

--- app/src/main/java/com/seurs/mareu/service/MeetingApiService.java
+++ app/src/main/java/com/seurs/mareu/service/MeetingApiService.java
@@ -6,5 +6,6 @@ import java.util.List;
 
 public interface MeetingApiService {
     List<Meeting> getMeetings();
+    void onAddMeeting(Meeting meeting);
     void onDeleteMeeting(Meeting meeting);
 }
