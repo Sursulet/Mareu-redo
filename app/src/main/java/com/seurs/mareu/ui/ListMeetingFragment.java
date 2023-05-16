98fa775596a1b93f0d6e9fc0d853ba7e78312caa
Sursulet <57486825+Sursulet@users.noreply.github.com>
2023-05-16 20:15:10 +0200

    Update ListMeetingFragment.java

--- app/src/main/java/com/seurs/mareu/ui/ListMeetingFragment.java
+++ app/src/main/java/com/seurs/mareu/ui/ListMeetingFragment.java
@@ -1,14 +1,24 @@
 package com.seurs.mareu.ui;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.google.android.material.snackbar.Snackbar;
 import com.seurs.mareu.R;
+import com.seurs.mareu.databinding.FragmentListMeetingBinding;
+import com.seurs.mareu.di.DI;
+import com.seurs.mareu.model.Meeting;
+import com.seurs.mareu.service.MeetingApiService;
+
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,6 +27,10 @@ import com.seurs.mareu.R;
  */
 public class ListMeetingFragment extends Fragment {
 
+    private MeetingApiService mService;
+    private List<Meeting> mMeetings;
+    private FragmentListMeetingBinding binding;
+    MeetingAdapter mAdapter;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -55,12 +69,53 @@ public class ListMeetingFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        mService = DI.getMeetingApiService();
+        mMeetings = mService.getMeetings();
+        mAdapter = new MeetingAdapter(mMeetings);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list_meeting, container, false);
+        binding = FragmentListMeetingBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.topAppBar.inflateMenu(R.menu.menu);
+        binding.topAppBar.setOnMenuItemClickListener(item -> {
+            if (item.getItemId() == R.id.filter_by_date) {
+                return true;
+            } else if (item.getItemId() == R.id.filter_by_place) {
+                return true;
+            }
+            return false;
+        });
+        binding.onAddMeeting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(ListMeetingFragment.this).navigate(R.id.action_ListMeetingFragment_to_AddMeetingFragment);
+                Snackbar.make(view, "Add a new meeting", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+            }
+        });
+
+        binding.meetingList.setLayoutManager(new LinearLayoutManager(getContext()));
+        binding.meetingList.setAdapter(mAdapter);
+        mAdapter.setOnClickListener(new MeetingAdapter.OnClickListener() {
+            @Override
+            public void onDeleteClick(int position) {
+                mService.onDeleteMeeting(mMeetings.get(position));
+                mAdapter.notifyItemRemoved(position);
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
