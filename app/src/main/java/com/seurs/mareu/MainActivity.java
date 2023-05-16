ee02e13a473754c2707b6deaae7dca1033f109ee
Sursulet <57486825+Sursulet@users.noreply.github.com>
2023-05-16 18:51:27 +0200

    Update MainActivity.java

--- app/src/main/java/com/seurs/mareu/MainActivity.java
+++ app/src/main/java/com/seurs/mareu/MainActivity.java
@@ -3,72 +3,27 @@ package com.seurs.mareu;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.android.material.appbar.MaterialToolbar;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.seurs.mareu.databinding.ActivityMainBinding;
-import com.seurs.mareu.di.DI;
-import com.seurs.mareu.model.Meeting;
-import com.seurs.mareu.service.MeetingApiService;
-import com.seurs.mareu.ui.MeetingAdapter;
-
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
-    private MeetingApiService mService;
-
-    MaterialToolbar mToolbar;
-    FloatingActionButton mFab;
-    RecyclerView mRecyclerview;
-
-    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        mService = DI.getMeetingApiService();
-
-        mToolbar = findViewById(R.id.topAppBar);
-        setSupportActionBar(mToolbar);
-        //mToolbar.setOverflowIcon(ContextCompat.getDrawable(this,R.drawable.baseline_filter_list_24));
-
-        mFab = findViewById(R.id.on_add_meeting);
-        mFab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Add a new meeting", Snackbar.LENGTH_LONG).setAction("Action", null).show();
-            }
-        });
-
-        mRecyclerview = findViewById(R.id.meeting_list);
-        mRecyclerview.setLayoutManager(new LinearLayoutManager(this));
-        List<Meeting> mMeetings = mService.getMeetings();
-        MeetingAdapter mAdapter = new MeetingAdapter(mMeetings);
-        mRecyclerview.setAdapter(mAdapter);
-        mAdapter.setOnClickListener(new MeetingAdapter.OnClickListener() {
-            @Override
-            public void onDeleteClick(int position) {
-                mService.onDeleteMeeting(mMeetings.get(position));
-                mAdapter.notifyItemRemoved(position);
-            }
-        });
+        setSupportActionBar(binding.topAppBar);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu,menu);
+        getMenuInflater().inflate(R.menu.menu, menu);
         return true;
     }
 
