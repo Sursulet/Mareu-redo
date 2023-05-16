f2fa4348e6080706b7468cec2b147bb4950c65cb
Sursulet <57486825+Sursulet@users.noreply.github.com>
2023-05-16 20:12:01 +0200

    Update MainActivity.java

--- app/src/main/java/com/seurs/mareu/MainActivity.java
+++ app/src/main/java/com/seurs/mareu/MainActivity.java
@@ -1,10 +1,7 @@
 package com.seurs.mareu;
 
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.seurs.mareu.databinding.ActivityMainBinding;
@@ -17,24 +14,5 @@ public class MainActivity extends AppCompatActivity {
 
         ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
-        setSupportActionBar(binding.topAppBar);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        int itemId = item.getItemId();
-        if (itemId == R.id.filter_by_date) {
-            return true;
-        } else if (itemId == R.id.filter_by_place) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
