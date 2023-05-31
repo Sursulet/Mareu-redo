ead9e1498c3fa5719e7234875c39279ccc014f4d
Sursulet <57486825+Sursulet@users.noreply.github.com>
2023-05-31 20:47:45 +0200

    Update DummyMeetingGenerator.java

--- app/src/main/java/com/seurs/mareu/service/DummyMeetingGenerator.java
+++ app/src/main/java/com/seurs/mareu/service/DummyMeetingGenerator.java
@@ -12,13 +12,28 @@ public class DummyMeetingGenerator {
         return new ArrayList<>(DUMMY_MEETINGS);
     }
 
-    private static final List<String> DUMMY_MAILS = Arrays.asList("xyz@example", "abc@example", "uvw@example.com");
+    private static final List<String> DUMMY_MAILS = Arrays.asList("xyz@example.com", "abc@example.com", "uvw@example.com");
     public static List<Meeting> DUMMY_MEETINGS = Arrays.asList(
-            new Meeting("09:00", "Meeting A", "26/02/2020", DUMMY_MAILS),
-            new Meeting("10:00", "Meeting B", "28/03/2020", DUMMY_MAILS),
-            new Meeting("11:00", "Meeting C", "27/02/2020", DUMMY_MAILS),
-            new Meeting("14:00", "Meeting D", "20/03/2020", DUMMY_MAILS),
-            new Meeting("09:00", "Meeting E", "29/02/2020", DUMMY_MAILS),
-            new Meeting("09:00", "Meeting F", "29/02/2020", DUMMY_MAILS)
+            new Meeting( "Meeting A", "PEACH", 0xffef9a9a, "26/02/2020","09:00", DUMMY_MAILS),
+            new Meeting( "Meeting B", "PEACH", 0xffa5d6a7, "28/03/2020","10:00", DUMMY_MAILS),
+            new Meeting( "Meeting C", "PEACH", 0xffa5d6a7, "27/02/2020","11:00", DUMMY_MAILS),
+            new Meeting( "Meeting D", "PEACH", 0xffef9a9a, "20/03/2020","14:00", DUMMY_MAILS),
+            new Meeting( "Meeting E", "PEACH", 0Xff90caf9, "29/02/2020","09:00", DUMMY_MAILS),
+            new Meeting( "Meeting F", "PEACH", 0xfffff59d, "29/02/2020","09:00", DUMMY_MAILS)
+    );
+
+    static List<Integer> generatePlaces() {
+        return new ArrayList<>(DUMMY_PLACES);
+    }
+
+    public static List<Integer> DUMMY_PLACES = Arrays.asList(
+            0xffef9a9a,
+            0xffa5d6a7,
+            0Xff90caf9,
+            0xfffff59d/*,
+            0xffffccbc,
+            Color.RED,
+            Color.GREEN,
+            Color.YELLOW*/
     );
 }
