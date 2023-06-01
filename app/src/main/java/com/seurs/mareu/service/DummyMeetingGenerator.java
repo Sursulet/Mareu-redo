84449bc203d22ca32b7e6c1fbbea61e46d6f2cca
Sursulet <57486825+Sursulet@users.noreply.github.com>
2023-06-01 12:58:51 +0200

    Update DummyMeetingGenerator.java

--- app/src/main/java/com/seurs/mareu/service/DummyMeetingGenerator.java
+++ app/src/main/java/com/seurs/mareu/service/DummyMeetingGenerator.java
@@ -1,5 +1,6 @@
 package com.seurs.mareu.service;
 
+import com.seurs.mareu.R;
 import com.seurs.mareu.model.Meeting;
 
 import java.util.ArrayList;
@@ -15,11 +16,11 @@ public class DummyMeetingGenerator {
     private static final List<String> DUMMY_MAILS = Arrays.asList("xyz@example.com", "abc@example.com", "uvw@example.com");
     public static List<Meeting> DUMMY_MEETINGS = Arrays.asList(
             new Meeting( "Meeting A", "PEACH", 0xffef9a9a, "26/02/2020","09:00", DUMMY_MAILS),
-            new Meeting( "Meeting B", "PEACH", 0xffa5d6a7, "28/03/2020","10:00", DUMMY_MAILS),
-            new Meeting( "Meeting C", "PEACH", 0xffa5d6a7, "27/02/2020","11:00", DUMMY_MAILS),
+            new Meeting( "Meeting B", "MARIO", 0xffa5d6a7, "28/03/2020","10:00", DUMMY_MAILS),
+            new Meeting( "Meeting C", "LUIGI", 0xffa5d6a7, "27/02/2020","11:00", DUMMY_MAILS),
             new Meeting( "Meeting D", "PEACH", 0xffef9a9a, "20/03/2020","14:00", DUMMY_MAILS),
-            new Meeting( "Meeting E", "PEACH", 0Xff90caf9, "29/02/2020","09:00", DUMMY_MAILS),
-            new Meeting( "Meeting F", "PEACH", 0xfffff59d, "29/02/2020","09:00", DUMMY_MAILS)
+            new Meeting( "Meeting E", "MARIO", 0Xff90caf9, "29/02/2020","09:00", DUMMY_MAILS),
+            new Meeting( "Meeting F", "LUIGI", 0xfffff59d, "29/02/2020","09:00", DUMMY_MAILS)
     );
 
     static List<Integer> generatePlaces() {
@@ -27,10 +28,10 @@ public class DummyMeetingGenerator {
     }
 
     public static List<Integer> DUMMY_PLACES = Arrays.asList(
-            0xffef9a9a,
-            0xffa5d6a7,
-            0Xff90caf9,
-            0xfffff59d/*,
+            R.color.red_200,
+            R.color.green_200,
+            R.color.blue_200,
+            R.color.yellow_200/*,
             0xffffccbc,
             Color.RED,
             Color.GREEN,
